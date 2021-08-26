daf055eeee4c5a6ecc4afd07d49b5b0bb991df30
Tarek <kheirtarek@hotmail.fr>
2021-08-26 18:29:28 +0200

    all tasks

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ *
+ * @head: linked list
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *size = *head;
+	int i = 0;
+    int j = 0;
+    int tab[512];
+
+    if (*head == NULL)
+        return (1);
+    
+    while (size)
+    {
+        tab[i] = size->n;
+        size = size->next;
+        i++;
+    }
+
+    while (j < i / 2)
+    {
+        if (tab[j] == tab[i - j - 1])
+            j++;
+        else
+            return (0);
+    }
+
+    return (1);
+}
