d4871f1d47fe2bb930a2b9bf3ef0f9693ebcf0cd
Tarek <kheirtarek@hotmail.fr>
2021-10-08 12:05:37 +0200

    fix 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,42 +2,15 @@
 """Lockboxes Module"""
 
 
-def checkKeys(keys, box):
-    """checks if the keys of the safe are not similar to the existing keys"""
-
-    new_keys = []
-
-    for k in box:
-        if k not in keys:
-            new_keys.append(k)
-
-    return new_keys
-
-
 def canUnlockAll(boxes):
-    """method that determines if all the boxes can be opened."""
+    """Checks if all boxes can be unlocked starting from the key
+    contained in box 0"""
+    opened = [0]
+    numberOfBoxes = len(boxes)
 
-    size = len(boxes)
-
-    if size == 0:
-        return False
-
-    keys = boxes[0]
-    boxes[0] = [-1]
-
-    while keys:
-
-        new_keys = []
-
-        for key in keys:
-
-            if boxes[key] != [-1]:
-                new_keys += checkKeys(keys, boxes[key])
-                boxes[key] = [-1]
-
-        keys = new_keys
-
-    if boxes.count([-1]) == size:
-        return True
-
-    return False
+    for box in opened:
+        if box < numberOfBoxes:
+            for newKey in boxes[box]:
+                if newKey not in opened and 0 < newKey < numberOfBoxes:
+                    opened.append(newKey)
+    return True if len(opened) >= numberOfBoxes else False
