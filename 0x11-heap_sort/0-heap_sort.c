7b0c2aa5e87171193fe10bed09f447581c19d6d0
Tarek <kheirtarek@hotmail.fr>
2021-11-25 14:13:43 +0100

    all

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -12,11 +12,11 @@
 
 void swap_values(int **array, ssize_t i1, ssize_t i2)
 {
-    int tmp;
+	int tmp;
 
-    tmp = (*array)[i1];
-    (*array)[i1] = (*array)[i2];
-    (*array)[i2] = tmp;
+	tmp = (*array)[i1];
+	(*array)[i1] = (*array)[i2];
+	(*array)[i2] = tmp;
 }
 
 /**
@@ -30,28 +30,28 @@ void swap_values(int **array, ssize_t i1, ssize_t i2)
 
 void heap_sort_sift_down(int *array, size_t size, size_t start, size_t end)
 {
-    size_t root = start, child, swap;
+	size_t root = start, child, swap;
 
-    while (root * 2 + 1 <= end)
-    {
-        child = root * 2 + 1;
-        swap = root;
+	while (root * 2 + 1 <= end)
+	{
+		child = root * 2 + 1;
+		swap = root;
 
-        if (array[swap] < array[child])
-            swap = child;
-        if (child + 1 <= end && array[swap] < array[child + 1])
-            swap = child + 1;
-        if (swap == root)
-        {
-            return;
-        }
-        else
-        {
-            swap_values(&array, root, swap);
-            print_array(array, size);
-            root = swap;
-        }
-    }
+		if (array[swap] < array[child])
+			swap = child;
+		if (child + 1 <= end && array[swap] < array[child + 1])
+			swap = child + 1;
+		if (swap == root)
+		{
+			return;
+		}
+		else
+		{
+			swap_values(&array, root, swap);
+			print_array(array, size);
+			root = swap;
+		}
+	}
 }
 
 /**
@@ -63,19 +63,19 @@ void heap_sort_sift_down(int *array, size_t size, size_t start, size_t end)
 
 void heap_sort(int *array, size_t size)
 {
-    size_t end = size - 1, start;
+	size_t end = size - 1, start;
 
-    if (size < 2)
-        return;
+	if (size < 2)
+		return;
 
-    for (start = (size - 2) / 2; (int)start >= 0; start--)
-        heap_sort_sift_down(array, size, start, size - 1);
+	for (start = (size - 2) / 2; (int)start >= 0; start--)
+		heap_sort_sift_down(array, size, start, size - 1);
 
-    while (end > 0)
-    {
-        swap_values(&array, end, 0);
-        print_array(array, size);
-        end--;
-        heap_sort_sift_down(array, size, 0, end);
-    }
+	while (end > 0)
+	{
+		swap_values(&array, end, 0);
+		print_array(array, size);
+		end--;
+		heap_sort_sift_down(array, size, 0, end);
+	}
 }
