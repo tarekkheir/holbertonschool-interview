accdc48b9aff9fb8ed85aa344f728fe13e03281a
Tarek <kheirtarek@hotmail.fr>
2022-01-16 20:31:36 +0100

    task 0

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds a loop in a linked list
+ *
+ * @head: pointer to head node
+ *
+ * Return: a pointer to the head node of the reversed list
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+    listint_t *slow = head;
+    listint_t *fast = head;
+
+    if (head && head->next)
+    {
+        while (slow && fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+            {
+                slow = head;
+                while (slow != fast)
+                {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return (slow);
+            }
+        }
+    }
+
+    return (NULL);
+}
